f25f8619bd977804ea17a735619f52387a5a7292
samuelehis <samuel.ehis@outlook.com>
2022-05-22 21:29:54 +0100

    added a random list to guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -1,9 +1,18 @@
+import random
 
+#generate a random 5 numbers between 1 and 100
+randomlist = random.sample(range(1, 100), 5)
+print(randomlist)
 
-our_fav_number = "10"
+#randomly select a number from the 5 numbers above
+our_fav_number = random.choice(randomlist)
 
-guess_num = input("Please enter your guessing number : ")
+user_name = input("Please enter your name : ")
 
+#accept number from the player
+guess_num = input("Hi " + user_name + " please enter your choice number : ")
+
+#convert the player's number from a string to an integer
 guess_num = int(guess_num)
 
 #print (guess_num)
@@ -11,14 +20,14 @@ guess_num = int(guess_num)
 try: 
     guess_num = int(guess_num)
 
-    if guess_num == 10:
-        print ("Congratulations! You guessed the number right. Our guess number is : " + str(guess_num))
+    if guess_num == our_fav_number:
+        print ("Congratulations! You guessed the number right. Our guess number is : " + str(our_fav_number))
 
-    elif guess_num < 10: 
-            print("Ouch! Your guess number is "+ str(guess_num) + " which is less than the number you entered.")
+    elif guess_num < our_fav_number: 
+            print("Ouch! Our selected number is "+ str(our_fav_number) + " is less than the number you guessed.")
 
     else:
-        print("Ouch! Your guess number is " + + str(guess_num) + " which is higher than the number you entered " )
+        print("Ouch! The selected number is " + str(our_fav_number) + " is higher than the number you guessed " )
 
 except: 
         print("Sorry! That is not a number")
