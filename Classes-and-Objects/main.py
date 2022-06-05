269e3013f50e1f5df7f4af09df0f962d55453089
samuelehis <samuel.ehis@outlook.com>
2022-06-05 18:31:16 +0100

    classes and objects

---
+++ Classes-and-Objects/main.py
@@ -0,0 +1,18 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+        
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -5,12 +5,24 @@
 
 def read_file_content(filename):
     # [assignment] Add your code here 
-    
-    return "Hello World"
+    with open('python-files\Reading-Text-Files\story.txt') as f:
+        file_content = f.read()
 
+    return "file_content"
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
+    counts = dict()
+    words = str.split(text)
 
-    return {"as": 10, "would": 20}
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+
+        else:
+            counts[word] = 1
+
+        return count
+        
+    #return {"as": 10, "would": 20}
